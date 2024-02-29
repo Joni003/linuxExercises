d80ed95c511f7653fcda6c7ba2c95c5cbd01e8a0
Jon Karanezi <j.karanezi003@gmail.com>
2024-02-29 11:48:50 -0600

    added mean script

--- mean.sh
+++ mean.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <column> [file.csv]" 1>&2
+    exit 0
+fi    
+
+column=$1
+csv_file=$2
+
+# Calculate Mean
+mean=$(cut -d',' -f$column "$csv_file" | tail -n +2 | {
+	   sum=0
+	   count=0
+	   while read -r line; do
+	       ((count++))
+	       sum=$(echo "scale=4; $sum + $line" | bc)
+	   done
+	   mean=$(echo "scale=4; $sum / $count" | bc)
+	   echo $mean
+       })
+echo "Mean of column $column: $mean"
