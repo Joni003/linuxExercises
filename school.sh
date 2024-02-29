7c00c12f818aa7f37b434c46159cdf5d3c9989a3
Jacob Stugelmeyer <jstugelmeyer@wisc.edu>
2024-02-29 12:08:47 -0600

    didn't need stdout in the second pipeline.

--- school.sh
+++ school.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" stdout | cut -d ',' -f 7 |  { sum=0; sum_n=0; avg=0; while read n; do sum=$((sum + n)); sum_n=$((sum_n +\
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 |  { sum=0; sum_n=0; avg=0; while read n; do sum=$((sum + n)); sum_n=$((sum_n +\
  1)); avg=$((sum / sum_n)); done; echo $avg; }
