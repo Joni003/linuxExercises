20026ebc0be99bd522c0dba55014d0642b13480b
Jingzhe Zhang <jingzhedeyouxiang@126.com>
2024-02-29 12:47:22 -0600

    Finished five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+mkdir -p "five"
+
+for ((i=1; i<=5; i++)); do
+    subdir="five/dir$i";
+    mkdir $subdir;
+    for ((j=1; j<=4; j++)); do
+	filename="$subdir/file$j";
+	for ((k=1; k<=$j; k++)); do
+	    echo "$j" >> "$filename";
+	done
+    done
+done
+	    
