ab25b5754c696de2e257d8c1b2fdbc9cda1633f2
Eugene <ohba@wisc.edu>
2024-03-05 09:48:21 -0600

    Implemented it boys

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+
+# Assign arguments to variables
+dir=$1
+n=$2
+
+# Find and remove files larger than n bytes in the specified directory
+find "$dir" -type f -size +"${n}c" -exec rm {} +
